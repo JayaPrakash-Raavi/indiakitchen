52b2a6d22bbe0f69d9eb7e019fcde4e14dcb33b9
JayaPrakash-Raavi <jpn9121@gmail.com>
2025-01-20 03:38:46 -0600

    Landing image added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
+import "./Hero.css";
+import backgroundImage from "../assets/images/landing_image.webp"; // Import the image
 
 const Hero = () => {
   return (
-    <section className="hero">
-      <h1>Welcome to</h1>
-      <h2>India Kitchen</h2>
-      <div className="buttons">
-        
-        <button className="explore-btn"><Link className="explore-btn" to="/menu">Menu</Link></button>
-        <button className="contact-btn"><Link className="explore-btn" to="/contact">Contact Us</Link></button>
+    <section
+      className="hero"
+      style={{
+        backgroundImage: `url(${backgroundImage})`, // Use the imported image
+      }}
+    >
+      <div className="overlay">
+        <div className="content">
+          <h1>Welcome to</h1>
+          <h2>India Kitchen</h2>
+          <div className="buttons">
+            <button className="explore-btn">Explore Menu</button>
+            <button className="contact-btn">Contact Us</button>
+          </div>
+        </div>
       </div>
     </section>
   );
